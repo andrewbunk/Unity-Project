3f3960c63c0ebfa36550f5b16aac67533f256c89
Andrew Bunk <andrewbunk89@gmail.com>
2020-09-07 22:44:51 -0700

    Initial commit

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    public Rigidbody2D rb2;
+    public GameObject PlayerMovement;
+    public Vector2 playerposition;
+    public float velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerposition = transform.parent.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            rb2.transform.Translate(playerposition * 5f * Time.fixedDeltaTime);           
+        } 
+        
+    }
+}
